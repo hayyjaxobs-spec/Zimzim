c7aa6bae251aec5e6f8f0b075132d740ebe43877
hayyjaxobs-spec <hayyjaxobs@gmail.com>
2026-05-13 13:53:06 +0000

    commit

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
 @app.route('/sms', methods=['POST'])
 def sms():
-    resp = MessagingResponse()
-    resp.message("Hello from Zimzim SMS API!")
-    return str(resp)
+    to_number = request.form.get('To')
+    from_number = request.form.get('From')
+    body = request.form.get('Body')
+    
+    log_message = f"Received SMS from {from_number} to {to_number}: {body}"
+    print(log_message)
+    
+    # Save to file
+    with open('sms_codes.txt', 'a') as f:
+        f.write(log_message + '\n')
+    
+    if to_number == '+12487522561':
+        resp = MessagingResponse()
+        resp.message("OK")
+        return str(resp)
+    else:
+        return 'OK'  # Ignore SMS to other numbers
 
 if __name__ == '__main__':
     app.run(debug=True)
